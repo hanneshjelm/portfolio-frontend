a0796998c7e4f04c423a300052013e4a99de921e
Hannes Hjelm <hannes.hjelm1@gmail.com>
2025-09-20 13:28:39 +0200

    Added css for hero section

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 
 import './App.css'
 import NavBar from "./components/navbar/NavBar.tsx";
+import Hero from "./components/hero/Hero.tsx";
 
 function App() {
   return (
       <div>
           <NavBar/>
+          <Hero/>
       </div>
   )
 }
