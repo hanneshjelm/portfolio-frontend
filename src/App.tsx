7a3ce46426d2b2b0a02888afe1aa2fed5aac0e0e
Hannes Hjelm <hannes.hjelm1@gmail.com>
2025-09-20 14:45:25 +0200

    Changed button style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-
-
 import './App.css'
 import NavBar from "./components/navbar/NavBar.tsx";
 import Hero from "./components/hero/Hero.tsx";
+import About from "./components/about/About.tsx";
 
 function App() {
   return (
       <div>
           <NavBar/>
           <Hero/>
+          <About/>
       </div>
   )
 }
